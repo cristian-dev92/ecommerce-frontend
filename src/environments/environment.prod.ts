5f5331bef204a9f1e3e686619b7c6337b80c4a4c
Cristian Alhambra <cristianalh@gmail.com>
2026-04-12 20:00:24 +0200

    Add environments to conect render

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ecommerce-backend-z7r5.onrender.com/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://ecommerce-backend-z7r5.onrender.com/api'
+};
