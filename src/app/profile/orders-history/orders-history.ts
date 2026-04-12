1d1782423f845f361918a9781e2a88b74594aaed
Cristian Alhambra <cristianalh@gmail.com>
2026-04-12 19:50:04 +0200

    redesign of orders-history

--- src/app/profile/orders-history/orders-history.ts
+++ src/app/profile/orders-history/orders-history.ts
@@ -7,12 +7,14 @@ import { Order } from '../../models/order';
   standalone: true,
   selector: 'app-orders-history',
   templateUrl: './orders-history.html',
+  styleUrl: './orders-history.css',
   imports: [CommonModule]
 })
 export class OrdersHistory {
 
   orders = signal<Order[]>([]);
   ordersService = inject(OrdersService);
+item: any;
 
   ngOnInit() {
     this.loadOrders();
